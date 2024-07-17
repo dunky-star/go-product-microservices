56d5f5d58287c44c426274b2a3bef186ca2637ae
dunky-star <dunkygeoffrey39@gmail.com>
2024-07-17 18:49:02 -0500

    Dcoumenting the API with go-swagger

--- main.go
+++ main.go
@@ -12,8 +12,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
 func main() {
     l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	// create the handlers
 	hProducts := handlers.NewProducts(l)
 
 	// Using Gorilla Mux and subrouters for routing.
@@ -32,14 +35,17 @@ func main() {
 	putRouter.Use(hProducts.MiddlewareValidateProduct)
 	
 
+	// create a new server
 	s := &http.Server{
-		Addr:         ":9090",
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+		Addr:         ":9090",      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func(){
 		err := s.ListenAndServe()
 		if err != nil{
@@ -48,14 +54,17 @@ func main() {
 
 	}()
 	
-	sigChan := make(chan os.Signal)
-    signal.Notify(sigChan, os.Interrupt)
-    signal.Notify(sigChan, os.Kill)
-    sig := <- sigChan
-    log.Println("Received terminate, graceful shutdown", sig)
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt)
+    signal.Notify(c, os.Kill)
 
-    tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-    defer cancel()
-	s.Shutdown(tc)
+    // Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+    // gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 
 }
